ac8b3d58249d054af23153aef736e6214545cd63
noamloya <noamloya@gmail.com>
2020-02-20 22:22:02 +0200

    added stuff to solve b and ran end to end

--- 2020-Qualification/naive.py
+++ 2020-Qualification/naive.py
@@ -1,7 +1,9 @@
 import numpy as np
 import input_parser
 import utils
-res = input_parser.parse("./inputs/a_example.txt")
+import solve_b
+
+res = input_parser.parse("./inputs/b_read_on.txt")
 days = int(res['D'])
 books_num = int(res['B'])
 libraries_num = int(res['L'])
@@ -17,16 +19,15 @@ days_left_for_capacity = np.full(library_capacity.shape, days) - library_capacit
 
 library_time_score = days_left_for_capacity - signup_time_for_library
 
-
 # libraries_num =
 
 
 # books_output = np.argsort(, axis=0)
 
 
-
-
-libraries_sort = np.argsort(library_time_score)[::-1]
+(libraries_sort, books_sort, num_of_books_in_library, books_per_day_from_lib,
+ days, signup_time_for_library) = solve_b.solve_b()
+# libraries_sort = np.argsort(library_time_score)[::-1]
 libraries_end_date = utils.get_libraries_signup_end_date(libraries_sort, signup_time_for_library)
 
 # print (book_ids_for_library )
@@ -34,5 +35,6 @@ libraries_end_date = utils.get_libraries_signup_end_date(libraries_sort, signup_
 # print(get_books_scores())
 
 # def score_per_library():
-utils.print_output(libraries_sort, book_ids_for_library, num_of_books_in_library, books_per_day_from_lib,
-                 libraries_end_date, days, 'out.txt', signup_time_for_library)
+print(books_sort)
+utils.print_output(libraries_sort, books_sort, num_of_books_in_library, books_per_day_from_lib,
+                   libraries_end_date, days, 'out.txt', signup_time_for_library)
--- 2020-Qualification/solve_b.py
+++ 2020-Qualification/solve_b.py
@@ -1,21 +1,15 @@
 import scoring
-from input_parser import parse
+import utils
 import numpy as np
 
 
 def solve_b():
-    res = parse("inputs/b_read_on.txt")
-    days = int(res['D'])
-    books_num = int(res['B'])
-    libraries_num = int(res['L'])
-    books_scores = np.asarray(res['scores'])
-
-    num_of_books_in_library = np.asarray(res['books_in_library'])
-    signup_time_for_library = np.asarray(res['signup_time_for_library'])
-    books_per_day_from_lib = np.asarray(res['books_per_day_from_lib'])
-    book_ids_for_library = np.asarray(res['book_ids_for_library'])
+    (days, books_num, libraries_num, books_scores, num_of_books_in_library, signup_time_for_library,
+     books_per_day_from_lib, book_ids_for_library) = utils.parse_file("inputs/b_read_on.txt")
 
     sorted_libs = np.argsort(signup_time_for_library)
-    print(sorted_libs)
-    print(signup_time_for_library[sorted_libs])
-    return sorted_libs, books_per_day_from_lib
+    # print(sorted_libs)
+    # print(signup_time_for_library[sorted_libs])
+
+    return (sorted_libs, book_ids_for_library, num_of_books_in_library, books_per_day_from_lib,
+            days, signup_time_for_library)
