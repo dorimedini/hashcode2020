98159b3b59457b1284a537c9c5f3bc9342d5f52f
dorimedini <dorimedini@gmail.com>
2020-02-20 22:49:12 +0200

    Bugfix in solve_d
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- 2020-Qualification/solve_d.py
+++ 2020-Qualification/solve_d.py
@@ -2,9 +2,9 @@ from utils import parse_file
 import numpy as np
 
 
-def solve_d():
+def solve():
     (days, books_num, libraries_num, books_scores, num_of_books_in_library, signup_time_for_library,
-     books_per_day_from_lib, book_ids_for_library) = parse_file("inputs/b_read_on.txt")
+     books_per_day_from_lib, book_ids_for_library) = parse_file("inputs/d_tough_choices.txt")
     sorted_libs = np.flip(np.argsort([len(x) for x in book_ids_for_library]))
     return (sorted_libs, book_ids_for_library, num_of_books_in_library, books_per_day_from_lib,
             days, signup_time_for_library)
