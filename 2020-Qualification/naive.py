ba921a217112c5ab433c968c77a3fbd368580b70
noamloya <noamloya@gmail.com>
2020-02-20 21:50:47 +0200

    added print function

--- 2020-Qualification/naive.py
+++ 2020-Qualification/naive.py
@@ -1,12 +1,7 @@
-
-
-
-
-
 import numpy as np
 import input_parser
 
-res = input_parser.parse("./input-files/a_example.txt")
+res = input_parser.parse("./inputs/a_example.txt")
 days = int(res['D'])
 books_num = int(res['B'])
 libraries_num = int(res['L'])
@@ -17,18 +12,13 @@ signup_time_for_library = np.asarray(res['signup_time_for_library'])
 books_per_day_from_lib = np.asarray(res['books_per_day_from_lib'])
 book_ids_for_library = np.asarray(res['book_ids_for_library'])
 
-
-library_capacity = np.ceil(np.divide(num_of_books_in_library, books_per_day_from_lib ))
+library_capacity = np.ceil(np.divide(num_of_books_in_library, books_per_day_from_lib))
 days_left_for_capacity = np.full(library_capacity.shape, days) - library_capacity
 
 library_time_score = days_left_for_capacity - signup_time_for_library
 
 
 # libraries_num =
-
-
-
-
 
 
 # books_output = np.argsort(, axis=0)
@@ -40,7 +30,7 @@ def get_books_scores(book_ids_for_library, books_scores):
     for books_in_lib in book_ids_for_library:
         books_scores_in_library = books_scores[books_in_lib]
         lib_scores.append(books_scores_in_library)
-        print (books_scores_in_library)
+        print(books_scores_in_library)
     return lib_scores
 
 
@@ -50,34 +40,46 @@ def libraries_signup_end_date(libraries_sort):
     accumulated_days = np.add.accumulate(libraries_latancies_by_order)
     return accumulated_days
 
+
 # calculate how much books each library can scan according to specficic order
 def number_of_libraries_to_sign_up(libraries_sort, days):
     accumulated_days = libraries_signup_end_date(libraries_sort)
-    accumulated_days = np.append(accumulated_days, days+1)
+    accumulated_days = np.append(accumulated_days, days + 1)
     return np.argmax(accumulated_days > days)
 
 
-def get_num_of_scanned_books_per_library(num_of_books_for_shipment_per_library, books_per_day_from_lib, libraries_end_date, days):
+def get_num_of_scanned_books_per_library(num_of_books_for_shipment_per_library, books_per_day_from_lib,
+                                         libraries_end_date, days):
     # TODO add number of actual books
-    number_days_left_per_library = np.maximum(np.full(libraries_end_date.shape, days) - libraries_end_date , 0)
-    number_of_books_sent_for_library = np.minimum(number_days_left_per_library * books_per_day_from_lib, num_of_books_for_shipment_per_library)
+    number_days_left_per_library = np.maximum(np.full(libraries_end_date.shape, days) - libraries_end_date, 0)
+    number_of_books_sent_for_library = np.minimum(number_days_left_per_library * books_per_day_from_lib,
+                                                  num_of_books_for_shipment_per_library)
     return number_of_books_sent_for_library
 
 
-def
+def print_output(library_order, book_order_per_library, num_of_books_for_shipment_per_library, books_per_day_from_lib,
+                 libraries_end_date, days, output_file):
+    # Prints the output as requested.
+    with open(output_file, "w") as f:
+        num_of_scanned_books_per_library = get_num_of_scanned_books_per_library(num_of_books_for_shipment_per_library,
+                                                                                books_per_day_from_lib,
+                                                                                libraries_end_date, days)
+        num_of_libraries = number_of_libraries_to_sign_up(library_order, days)
+
+        f.write(str(num_of_libraries) + '\n')
+        for library, num_of_scanned_books, books_to_scan in zip(library_order, num_of_scanned_books_per_library,
+                                                                book_order_per_library):
+            f.write(str(library) + " " + str(num_of_scanned_books) + '\n')
+            f.write(' '.join([str(b) for b in books_to_scan]) + '\n')
 
 
 libraries_sort = np.argsort(library_time_score)[::-1]
 libraries_end_date = libraries_signup_end_date(libraries_sort)
-
-
-
 
 # print (book_ids_for_library )
 
 # print(get_books_scores())
 
 # def score_per_library():
-
-
-
+print_output(libraries_sort, book_ids_for_library, num_of_books_in_library, books_per_day_from_lib,
+                 libraries_end_date, days, 'out.txt')
