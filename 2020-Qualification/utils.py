255dd168eb37d7132653da30009ea1da74d6d078
ori zviran <ori.zviran@gmail.com>
2020-02-20 21:45:46 +0200

    intial library scoring

--- 2020-Qualification/utils.py
+++ 2020-Qualification/utils.py
@@ -7,3 +7,24 @@ def total_library_scores(dataset):
         scores = np.array([dataset['scores'][bID] for bID in dataset['book_ids_for_library'][l]])
         r.append(np.sum(scores))
     return r
+
+
+def initial_library_scores(dataset):
+    '''gets a dataset and computes the initial score per library'''
+    signup_time = dataset['signup_time_for_library']
+    total_time = dataset['D']
+    num_books = dataset['books_in_library']
+    books_per_day = dataset['books_per_day_from_lib']
+    books_in_libraries = dataset['book_ids_for_library']
+    scores = []
+    for library in range(dataset['L']):
+        books_scores = np.sort(np.array([(dataset['scores'][bID], bID) for bID in books_in_libraries[library]]))
+        if signup_time[library]>total_time:
+            scores.append((-1,[]))
+        else:
+            if (total_time-signup_time[library])*books_per_day[library]<num_books[library]:
+                books_score =books_score[:(total_time-signup_time[library])*books_per_day[library]]
+            score = np.sum([score for (score, book) in books_scores]) / books_per_day[library]
+            books = [book for (score, book) in books_scores]
+            scores.append((score, books))
+    return scores
